b21e62f39db30a7ca779b399abe8f3279e6408b3
Nidhi-03 <141109364+Nidhi-03@users.noreply.github.com>
2023-08-12 13:25:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starter-abcd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-mcmcbi)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import{ Hero } from "./hero";
+
+export const HEROES:Hero[]=[
+  {id:1, avatar:'https://images.news18.com/ibnlive/uploads/2023/07/darshan-raval-interview.jpg', name:'Darshan Raval', field:'Music', info:'Darshan Raval is an Indian singer, composer, and songwriter. He is known for his work in different languages including Hindi, Gujarati, Punjabi and Bengali. In 2014, he participated in the StarPlus music reality show, Indias Raw Star, finishing as the first runner-up.'},
+
+  {id:2, avatar:'https://starsunfolded.com/wp-content/uploads/2014/06/Priyanka-Chopra-Miss-World-2000.jpg', name:'Priyanka Chopra', field:'Acting', info:'Chopra was the fifth Indian contestant to win Miss World, and the fourth to do so within seven years. She had enrolled in college, but left after winning the Miss World pageant. Chopra said that the Miss India and Miss World titles brought her recognition, and she began receiving offers for film roles.'},
+
+  {id:3, avatar:'https://m.media-amazon.com/images/I/71Vwn1KXApL.jpg', name:'M.S.Dhoni', field:'Cricket', info:'M.S. Dhoni, in full Mahendra Singh Dhoni, (born July 7, 1981, Ranchi, Bihar [now Jharkhand] state, India), Indian cricketer whose rise to prominence in the early 21st century culminated in his captaincy of the Indian national team that won the one-day Cricket World Cup in 2011.'},
+
+  {id:4, avatar:'https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/1/2022_1$largeimg_213980714.JPG', name:'Remo DSouza', field:'Dance', info:'Remo DSouza (born Ramesh Gopi Nair; 2 April 1974), is an Indian choreographer, film director, and producer based in Mumbai. He is best known for his work in films such as Yeh Jawaani Hai Deewani (2013) and Bajirao Mastani (2015). Over the course of his career spanning more than 25 years, DSouza has choreographed more than 100 films.'},
+
+];
